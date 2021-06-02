0f612232aa3d556c95f2b9b87f1ddc7cbeccae65
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-06-02 10:49:03 +0530

    To print the factorial of entered number.

---
+++ Factorial of entered number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a=1,n,i;
+   cin>>n;
+   for(i=1;i<=n;i++)
+   a=a*i;
+     {
+     	cout<<"factorial is: "<<n<<"\n"<<a;
+	 }
+	
+	return 0;
+}
